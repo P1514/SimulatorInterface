7a27db629cce3d3d723735911f4aa0c76e76a631
fcrsilva <f.silva@uninova.pt>
2017-11-14 19:32:18 +0000

    V1.1

--- src/general/TwitterAgent.java
+++ src/general/TwitterAgent.java
@@ -15,6 +15,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import twitter4j.*;
+import twitter4j.Query.ResultType;
 import twitter4j.conf.*;
 
 public class TwitterAgent {
@@ -62,87 +63,113 @@ public class TwitterAgent {
 		List<Status> results;
 		JSONArray output = new JSONArray();
 		account = account.substring(1, account.length() - 1);
-		query.setQuery("@" + account);
-		// query.setQuery("source:"+ account);
-		query.setCount(1000);
-		try {
+		int page = 1;
+			query.setQuery("@" + account);
+			query.setCount(100);
+			query.setResultType(Query.ResultType.recent);
+			// query.setQuery("source:"+ account);
+			try {
 
-			// results = twitter.getUserTimeline("@"+account,p);
-			Logger.getLogger(TwitterAgent.class.getName()).info("Getting Posts From Twitter, Please Wait...");
-			do {
+				// results = twitter.getUserTimeline("@"+account,p);
+				Logger.getLogger(TwitterAgent.class.getName()).info("Getting posts from twitter, please wait...");
+				do {
+					query.setMaxId(-1);
+					Logger.getLogger(TwitterAgent.class.getName()).info("Getting page: "+page);
+					queryresult = this.getTwitter().search(query);
+					results = queryresult.getTweets();
 
-				query.setCount(1000);
-				queryresult = this.getTwitter().search(query);
-				results = queryresult.getTweets();
 
-				if (results == null || results.isEmpty()) {
-					// TODO
-				}
+					for (Status status : results) {
 
-				for (Status status : results) {
+						twitterUser = status.getUser();
+						currentPostEpoch = status.getCreatedAt().getTime();
 
-					twitterUser = status.getUser();
-					currentPostEpoch = status.getCreatedAt().getTime();
+						JSONObject out = new JSONObject();
+						out.put("source", "twitter");
+						out.put("user_id", twitterUser.getId());
+						out.put("Fname", twitterUser.getName());
+						out.put("age", "");
+						out.put("gender", "");
+						out.put("postId", status.getId());
+						out.put("location", twitterUser.getLocation());
+						out.put("retweet", status.getRetweetCount());
+						out.put("account", account);
+						out.put("url", "");
+						out.put("mediaSpecificInfo", "true");
+						out.put("imgUrl", "");// take care
+						out.put("postEpoch", currentPostEpoch);
+						out.put("post", status.getText());
 
-					JSONObject out = new JSONObject();
-					out.put("source", "twitter");
-					out.put("user_id", twitterUser.getId());
-					out.put("Fname", twitterUser.getName());
-					out.put("age", "");
-					out.put("gender", "");
-					out.put("postId", status.getId());
-					out.put("location", twitterUser.getLocation());
-					out.put("retweet", status.getRetweetCount());
-					out.put("account", account);
-					out.put("url", "");
-					out.put("mediaSpecificInfo", "true");
-					out.put("imgUrl", "");// take care
-					out.put("postEpoch", currentPostEpoch);
-					out.put("post", status.getText());
+						try (Connection cnlocal = Server.connlocal();
+								PreparedStatement ps = cnlocal.prepareStatement(
+										"INSERT IGNORE INTO `sentimentposts`.`user` (`id`, `name`, `location`) VALUES (?, ?, ?)")) {
+							ps.setLong(1, twitterUser.getId());
+							ps.setString(2, twitterUser.getName());
+							ps.setString(3, twitterUser.getLocation());
+							ps.execute();
 
-					try (Connection cnlocal = Server.connlocal();
-							PreparedStatement ps = cnlocal.prepareStatement(
-									"INSERT IGNORE INTO `sentimentposts`.`user` (`id`, `name`, `location`) VALUES (?, ?, ?)")) {
-						ps.setLong(1, twitterUser.getId());
-						ps.setString(2, twitterUser.getName());
-						ps.setString(3, twitterUser.getLocation());
-						ps.execute();
+						} catch (SQLException | ClassNotFoundException e) {
+							Logger.getLogger(TwitterAgent.class.getName()).severe(e.getMessage());
+						}
+						if (status.getInReplyToStatusId() != -1) {
 
-					} catch (SQLException | ClassNotFoundException e) {
-						Logger.getLogger(TwitterAgent.class.getName()).severe(e.getMessage());
+							try (Connection cnlocal = Server.connlocal();
+									PreparedStatement ps1 = cnlocal.prepareStatement(
+											"INSERT INTO `sentimentposts`.`post` (`id`, `timestamp`, `message`, `likes`, `views`, `user_id`, `product`, `post_id`) VALUES (?, ?, ?, ?, ?, ?, ?,?) on duplicate key update\n"
+													+ "message=?, likes=?, views=?;")) {
+								ps1.setLong(1, status.getId());
+								ps1.setString(2, df.format(currentPostEpoch));
+								ps1.setString(3, status.getText());
+								ps1.setLong(4, status.getFavoriteCount());
+								ps1.setLong(5, status.getRetweetCount());
+								ps1.setLong(6, twitterUser.getId());
+								ps1.setString(7, account);
+								ps1.setLong(8, status.getInReplyToStatusId());
+								ps1.setString(9, status.getText());
+								ps1.setLong(10, status.getFavoriteCount());
+								ps1.setLong(11, status.getRetweetCount());
+								ps1.execute();
+
+							} catch (SQLException | ClassNotFoundException e) {
+								Logger.getLogger(TwitterAgent.class.getName()).severe(e.getMessage());
+							}
+						} else {
+							try (Connection cnlocal = Server.connlocal();
+									PreparedStatement ps1 = cnlocal.prepareStatement(
+											"INSERT INTO `sentimentposts`.`post` (`id`, `timestamp`, `message`, `likes`, `views`, `user_id`, `product`) VALUES (?, ?, ?, ?, ?, ?, ?) on duplicate key update\n"
+													+ "message=?, likes=?, views=?;")) {
+								ps1.setLong(1, status.getId());
+								ps1.setString(2, df.format(currentPostEpoch));
+								ps1.setString(3, status.getText());
+								ps1.setLong(4, status.getFavoriteCount());
+								ps1.setLong(5, status.getRetweetCount());
+								ps1.setLong(6, twitterUser.getId());
+								ps1.setString(7, account);
+								ps1.setString(8, status.getText());
+								ps1.setLong(9, status.getFavoriteCount());
+								ps1.setLong(10, status.getRetweetCount());
+								ps1.execute();
+
+							} catch (SQLException | ClassNotFoundException e) {
+								Logger.getLogger(TwitterAgent.class.getName()).severe(e.getMessage());
+							}
+						}
+						output.put(out);
+
 					}
+					Thread.sleep(1000);
+					page++;
+				} while ((query = queryresult.nextQuery()) != null);
 
-					try (Connection cnlocal = Server.connlocal();
-							PreparedStatement ps1 = cnlocal.prepareStatement(
-									"INSERT INTO `sentimentposts`.`post` (`id`, `timestamp`, `message`, `likes`, `views`, `user_id`, `product`) VALUES (?, ?, ?, ?, ?, ?, ?) on duplicate key update\n"
-											+ "message=?, likes=?, views=?;")) {
-						ps1.setLong(1, status.getId());
-						ps1.setString(2, df.format(currentPostEpoch));
-						ps1.setString(3, status.getText());
-						ps1.setLong(4, status.getFavoriteCount());
-						ps1.setLong(5, status.getRetweetCount());
-						ps1.setLong(6, twitterUser.getId());
-						ps1.setString(7, account);
-						ps1.setString(8, status.getText());
-						ps1.setLong(9, status.getFavoriteCount());
-						ps1.setLong(10, status.getRetweetCount());
-						ps1.execute();
-
-					} catch (SQLException | ClassNotFoundException e) {
-						Logger.getLogger(TwitterAgent.class.getName()).severe(e.getMessage());
-					}
-
-					output.put(out);
-				}
-				Thread.sleep(1000);
-			} while ((query = queryresult.nextQuery()) != null);
-			Logger.getLogger(TwitterAgent.class.getName()).log(Level.INFO, "Posts were retrieved! Total: "
-					+ output.length() + " Last Epoch Time: " + currentPostEpoch + " Epoch Date:  {0}",
-					new Date(currentPostEpoch).toString());
-		} catch (TwitterException ex) {
-			Logger.getLogger(TwitterAgent.class.getName()).severe(ex.getMessage());
-		}
-
+				Logger.getLogger(TwitterAgent.class.getName())
+						.log(Level.INFO,
+								"Posts were retrieved! Total: " + output.length() + " Last Epoch Time: "
+										+ currentPostEpoch + " Epoch Date:  {0}",
+								new Date(currentPostEpoch).toString());
+			} catch (TwitterException ex) {
+				Logger.getLogger(TwitterAgent.class.getName()).severe(ex.getMessage());
+			}
+		
 		return output;
 
 	}
